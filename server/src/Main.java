04a6c9ca6bd57185a7512171b50e618d91b7a00b
Tatsuhiko <tatsuhiko.k13@outlook.jp>
2023-07-05 01:15:38 +0900

    EX3-end

--- server/src/Main.java
+++ server/src/Main.java
@@ -7,5 +7,27 @@ class Main {
 
         System.out.println("コンパイル実行");
 
+       // long x = 65536 ;
+     
+        //float y = 4.2;
+
+       // (int) x ;
+
+        //System.out.println(x);
+        System.out.print("かぼちゃ");
+
+
+        int a = 369; //小数点がない　数字
+        long b = 369369L; //小数点がない　数字で末尾が　L
+        double c = 4.2; //小数点付きの数字
+        float d = 4.2F; //小数点付きの数字で末尾が　F
+        boolean e = true false; //true か false
+        char f = '私'; //引用符で囲まれた文字
+        string g = "java"; //二重引用符で囲まれた文字
+
+
+        
     }
+
+    
 }
