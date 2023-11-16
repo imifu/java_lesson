684f2e7fab8bc887e5aecce672c41d76939d8163
mami <mami.hgwr@gmail.com>
2023-11-16 16:50:24 +0900

    ３章練習問題

--- server/src/Main.java
+++ server/src/Main.java
@@ -3,23 +3,72 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) { //メインメソッド
-        int x = 5;
-        int y = 10;
-        String ans ="x+yは" + (x + y);
-        System.out.println(ans);
+    //文字列を取り出してみた
+      String value ="あいうえお";
+            //  System.out.println(value.charAt(1));
 
-        System.out.println("ようこそ占いの館へ");
-        System.out.println("名前を入力してください");
-        String name = new java.util.Scanner(System.in).nextLine();
-        System.out.println("年齢を入力してください");
-        String ageString = new java.util.Scanner(System.in).nextLine();
-        int age = Integer.parseInt(ageString);
-        int fortune = new java.util.Random().nextInt(4);
-        fortune++;
-       
-        System.out.println( age + "さいの" + name + "運気番号は" + fortune +"です。");
-        System.out.println( "1:大吉 2:中吉 3:吉 4:キョウ");
+      for(int i= 0; i < value.length(); i++){
+        System.out.println("1ばん"+value.charAt(i));
+      }
+
+    /*3-1
+     weight == 60;
+     (age1 +age2) *2 >60;
+     age % 2 ==1
+     String name =="湊";　正解 name.equals("湊")
+    */
+    //3-3
+    int isHungry = 1;
+    String food ="みかん";
+    System.out.println("こんにちは");
+    if(isHungry == 0){
+        System.out.println("お腹がいっぱいです。");}
+    else{
+        System.out.println(food +"をいただきます。");
+    };
+    System.out.println("ごちそうさまでした");
+        
+
+     //3-5
+     System.out.println("1検索 2登録 3削除 4変更");
+     
+     int selected = 5;
+     switch(selected){
+        case 1:
+             System.out.println("1検索");
+             break;
+        case 2:
+             System.out.println("2登録");
+             break;
+        case 3:
+             System.out.println("3削除");
+             break;
+        case 4:
+             System.out.println("4変更");
+             break;               
+     }
+
+     //3-6
+     System.out.println("数当てゲーム!!5回まで");
+     int ans = new java.util.Random().nextInt(9);
+
+     
+     for(int i =0 ; i<5 ;i++){
+         System.out.println("0から9の数字を入力してください");
+         int num =new java.util.Scanner(System.in).nextInt();
+         
+         if(num == ans){
+                     System.out.println("あたり");
+                     break; //あたりになったら抜ける
+         }else{
+                     System.out.println("はずれ");
+         }
+     }
+      System.out.println("おしまい");
+
+
+
+
+
     }
-
-
 }
